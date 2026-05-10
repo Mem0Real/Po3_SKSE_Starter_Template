ecbeacf1410b64d34176157291dcda00ea94e944
Mem0Real <juniourmimo@gmail.com>
2026-05-10 05:41:06 +0300

    Starter repo for SKSE developement using po3\'s CommonLibSSE

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+#include "PCH.h"
+
+void MessageHandler(SKSE::MessagingInterface::Message* a_message) {
+	if (a_message->type == SKSE::MessagingInterface::kDataLoaded) {
+		// Runs once the game reaches main menu
+		RE::ConsoleLog::GetSingleton()->Print("SavingGrace: Hello from the console");
+		logger::info("Hello from the log file!");
+	}
+}
+
+void InitializeLog()
+{
+	auto path = logger::log_directory();
+	if (!path) {
+		stl::report_and_fail("Failed to find standard logging directory"sv);
+		return;
+	}
+
+	*path /= fmt::format(FMT_STRING("{}.log"), Version::PROJECT);
+	auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+	auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+
+	log->set_level(spdlog::level::info);
+	log->flush_on(spdlog::level::info);
+
+	spdlog::set_default_logger(std::move(log));
+	spdlog::set_pattern("[%l] %v"s);
+
+	logger::info(FMT_STRING("{} v{}"), Version::PROJECT, Version::NAME);
+}
+
+
+extern "C" DLLEXPORT constinit auto SKSEPlugin_Version = []() {
+	SKSE::PluginVersionData v;
+	v.PluginVersion(Version::MAJOR);
+	v.PluginName("SavingGrace");
+	v.AuthorName("Mem0Real");
+
+	v.UsesAddressLibrary();
+	v.UsesUpdatedStructs();
+	v.CompatibleVersions({ SKSE::RUNTIME_SSE_LATEST });
+
+	return v;
+}();
+
+extern "C" DLLEXPORT bool SKSEAPI SKSEPlugin_Load(const SKSE::LoadInterface* a_skse)
+{
+	InitializeLog();
+
+	logger::info("Game version : {}", a_skse->RuntimeVersion());
+
+	SKSE::Init(a_skse);
+
+	const auto messaging = SKSE::GetMessagingInterface();
+	messaging->RegisterListener(MessageHandler);
+
+	return true;
+}
